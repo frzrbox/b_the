8effebe980ba4f4591d0ab7ab89ccec3a28691b0
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2021-06-17 22:19:17 -1000

    feat: toggles drawers on keydown

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,22 +1,28 @@
-const accordianDrawers = document.querySelectorAll('.accordian__drawers__item');
+const accordionDrawers = document.querySelectorAll('.accordion__drawers__item');
 
-accordianDrawers.forEach((drawer) => {
-	const drawerToggle = drawer.querySelector('.accordian__drawers__item__top');
-	const drawerBody = drawer.querySelector('.accordian__drawers__item__body');
-	const visibleClass = 'accordian__drawers__item__body--visible';
-	let isExpanded = !!drawerToggle.getAttribute('aria-expanded');
+function toggleDrawers(button, content) {
+	const visibleClass = 'accordion__drawers__item__body--visible';
+
+	button.toggleAttribute('aria-pressed');
+	button.toggleAttribute('aria-expanded');
+	content.classList.toggle(visibleClass);
+}
+
+accordionDrawers.forEach((drawer) => {
+	const drawerToggle = drawer.querySelector('.accordion__drawers__item__top');
+	const drawerBody = drawer.querySelector('.accordion__drawers__item__body');
 
 	drawerToggle.addEventListener('click', () => {
-		if (isExpanded) {
-			isExpanded = false;
-			drawerToggle.setAttribute('aria-pressed', false);
-			drawerToggle.setAttribute('aria-expanded', false);
-			drawerBody.classList.remove(visibleClass);
-		} else {
-			isExpanded = true;
-			drawerToggle.setAttribute('aria-pressed', true);
-			drawerToggle.setAttribute('aria-expanded', true);
-			drawerBody.classList.add(visibleClass);
+		toggleDrawers(drawerToggle, drawerBody);
+	});
+
+	drawerToggle.addEventListener('keydown', (event) => {
+		if (
+			event.key === ' ' ||
+			event.key === 'Enter' ||
+			event.key === 'Spacebar'
+		) {
+			toggleDrawers(drawerToggle, drawerBody);
 		}
 	});
 });
