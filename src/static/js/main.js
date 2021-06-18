15faa40ce918639a0a1845ca92c0dfc5e8433fa6
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2021-06-17 21:58:38 -1000

    feat: adds logic for accordian functionality

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,1 +1,22 @@
-console.log('connected this is a test');
+const accordianDrawers = document.querySelectorAll('.accordian__drawers__item');
+
+accordianDrawers.forEach((drawer) => {
+	const drawerToggle = drawer.querySelector('.accordian__drawers__item__top');
+	const drawerBody = drawer.querySelector('.accordian__drawers__item__body');
+	const visibleClass = 'accordian__drawers__item__body--visible';
+	let isExpanded = !!drawerToggle.getAttribute('aria-expanded');
+
+	drawerToggle.addEventListener('click', () => {
+		if (isExpanded) {
+			isExpanded = false;
+			drawerToggle.setAttribute('aria-pressed', false);
+			drawerToggle.setAttribute('aria-expanded', false);
+			drawerBody.classList.remove(visibleClass);
+		} else {
+			isExpanded = true;
+			drawerToggle.setAttribute('aria-pressed', true);
+			drawerToggle.setAttribute('aria-expanded', true);
+			drawerBody.classList.add(visibleClass);
+		}
+	});
+});
