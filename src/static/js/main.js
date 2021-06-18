930c2db17a7d38b7c2e4932f531d56516f55d83f
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2021-06-17 22:37:57 -1000

    edit: adds the arrow icon to the accordion logic

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,19 +1,24 @@
 const accordionDrawers = document.querySelectorAll('.accordion__drawers__item');
 
-function toggleDrawers(button, content) {
-	const visibleClass = 'accordion__drawers__item__body--visible';
+function toggleDrawers(button, content, arrow) {
+	const visibleContentClass = 'accordion__drawers__item__body--visible';
+	const visibleArrowClass = 'accordian__drawers__item__top__arrow--open';
 
 	button.toggleAttribute('aria-pressed');
 	button.toggleAttribute('aria-expanded');
-	content.classList.toggle(visibleClass);
+	content.classList.toggle(visibleContentClass);
+	arrow.classList.toggle(visibleArrowClass);
 }
 
 accordionDrawers.forEach((drawer) => {
 	const drawerToggle = drawer.querySelector('.accordion__drawers__item__top');
 	const drawerBody = drawer.querySelector('.accordion__drawers__item__body');
+	const arrowIcon = drawer.querySelector(
+		'.accordian__drawers__item__top__arrow'
+	);
 
 	drawerToggle.addEventListener('click', () => {
-		toggleDrawers(drawerToggle, drawerBody);
+		toggleDrawers(drawerToggle, drawerBody, arrowIcon);
 	});
 
 	drawerToggle.addEventListener('keydown', (event) => {
@@ -22,7 +27,7 @@ accordionDrawers.forEach((drawer) => {
 			event.key === 'Enter' ||
 			event.key === 'Spacebar'
 		) {
-			toggleDrawers(drawerToggle, drawerBody);
+			toggleDrawers(drawerToggle, drawerBody, arrowIcon);
 		}
 	});
 });
