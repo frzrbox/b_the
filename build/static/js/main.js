5b408fe5562dc7d1a41c6274cce17884c78d7533
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2021-06-18 00:15:30 -1000

    feat: base styles and functionality in

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -1,22 +1,33 @@
-const accordianDrawers = document.querySelectorAll('.accordian__drawers__item');
+const accordionDrawers = document.querySelectorAll('.accordion__drawers__item');
 
-accordianDrawers.forEach((drawer) => {
-	const drawerToggle = drawer.querySelector('.accordian__drawers__item__top');
-	const drawerBody = drawer.querySelector('.accordian__drawers__item__body');
-	const visibleClass = 'accordian__drawers__item__body--visible';
-	let isExpanded = !!drawerToggle.getAttribute('aria-expanded');
+function toggleDrawers(button, content, arrow) {
+	const visibleContentClass = 'accordion__drawers__item__body--visible';
+	const visibleArrowClass = 'accordion__drawers__item__top__arrow--open';
+
+	button.toggleAttribute('aria-pressed');
+	button.toggleAttribute('aria-expanded');
+	content.classList.toggle(visibleContentClass);
+	arrow.classList.toggle(visibleArrowClass);
+}
+
+accordionDrawers.forEach((drawer) => {
+	const drawerToggle = drawer.querySelector('.accordion__drawers__item__top');
+	const drawerBody = drawer.querySelector('.accordion__drawers__item__body');
+	const arrowIcon = drawer.querySelector(
+		'.accordion__drawers__item__top__arrow'
+	);
 
 	drawerToggle.addEventListener('click', () => {
-		if (isExpanded) {
-			isExpanded = false;
-			drawerToggle.setAttribute('aria-pressed', false);
-			drawerToggle.setAttribute('aria-expanded', false);
-			drawerBody.classList.remove(visibleClass);
-		} else {
-			isExpanded = true;
-			drawerToggle.setAttribute('aria-pressed', true);
-			drawerToggle.setAttribute('aria-expanded', true);
-			drawerBody.classList.add(visibleClass);
+		toggleDrawers(drawerToggle, drawerBody, arrowIcon);
+	});
+
+	drawerToggle.addEventListener('keydown', (event) => {
+		if (
+			event.key === ' ' ||
+			event.key === 'Enter' ||
+			event.key === 'Spacebar'
+		) {
+			toggleDrawers(drawerToggle, drawerBody, arrowIcon);
 		}
 	});
 });
